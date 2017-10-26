8c8dcb31be53c21a402aa2157ff3c41857a4344c
amatranga <amatranga892@gmail.com>
2017-10-25 21:44:10 -0700

    Remake application in jsx.
    It turns out that a few of the imports needed to use react-google-maps
    do not play nicely with TypeScript

---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RecentEarthQuake from './components/RecentEarthQuake';
+
+ReactDOM.render(<RecentEarthQuake />, document.getElementById('app'));
