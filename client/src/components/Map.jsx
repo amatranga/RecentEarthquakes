62270e538fc7b802c3a8197cd7dd58d8ef7c6d9b
amatranga <amatranga892@gmail.com>
2017-10-26 17:44:13 -0700

    Render Markers with labels
    Labels are hard-coded to always be shown. Next step is to make the
    labels turn on/off when the label is clicked

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -3,6 +3,19 @@ import { compose, withProps } from 'recompose';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 import GOOGLE_MAPS_JS_API_KEY from '../config/config';
 
+const findCenter = (geoLocationsArray, idx) => {
+  let total = 0;
+  let length = geoLocationsArray.length;
+  geoLocationsArray.forEach(location => {
+    total += location.geometry.coordinates[idx];
+  });
+  return total / length;
+}
+
+const click = () => {
+  console.log('why');
+}
+
 const Map = compose(
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_JS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
@@ -13,10 +26,18 @@ const Map = compose(
   withScriptjs,
   withGoogleMap
 )(props => {
-  console.log(props.earthquakes);
   return(
-  <GoogleMap defaultZoom={2} defaultCenter={{ lat: 0, lng: 0}}>
-    <Marker position={{ lat: 0, lng: 0}} />
+  <GoogleMap
+    defaultZoom={2}
+    defaultCenter={{ lat: findCenter(props.earthquakes, 1), lng: findCenter(props.earthquakes, 0)}} 
+  >
+    {props.earthquakes.map((earthquake, idx) => 
+      <Marker
+        key={idx}
+        position={{ lat: earthquake.geometry.coordinates[1], lng: earthquake.geometry.coordinates[0]}} 
+        defaultLabel={'' + earthquake.properties.mag}
+      />
+    )}
   ></GoogleMap>
 )});
 
